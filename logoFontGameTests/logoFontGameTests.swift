609f22478e93e3a9475db1e7a9b0721cc2e602e0
Eriri Matsui <eririmatsui@net74-dhcp182.sfc.keio.ac.jp>
2017-08-18 13:43:04 +0900

    Initial Commit

---
+++ logoFontGameTests/logoFontGameTests.swift
@@ -0,0 +1,36 @@
+//
+//  logoFontGameTests.swift
+//  logoFontGameTests
+//
+//  Created by Eriri Matsui on 2017/08/18.
+//  Copyright © 2017年 Eriri Matsui. All rights reserved.
+//
+
+import XCTest
+@testable import logoFontGame
+
+class logoFontGameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
